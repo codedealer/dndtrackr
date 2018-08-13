b615e958dbb1cc07fce37eff5974f5786b6035c2
Thomas Mello <work.mello@gmail.com>
2018-08-13 03:39:45 +0300

    optimize codebase size

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/functions'
 import firebaseui from 'firebaseui'
 import config from './config.json'
 import axios from 'axios'
